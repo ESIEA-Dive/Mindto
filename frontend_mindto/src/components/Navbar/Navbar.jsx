775cdd930a4a845839157fa07a5bf5272ee4dfa3
ESIEA-Dive <dive@et.esiea.fr>
2022-10-06 11:53:10 +0200

    Navbar Setup / Images Setup

--- frontend_mindto/src/components/Navbar/Navbar.jsx
+++ frontend_mindto/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {  } from 'react';
+//useState
+import { } from 'react-icons/';
 
+//import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <nav className='app__navbar'>
+    <div className='app__navbar-logo'>
+      <img src={images.logo} alt='logo'></img>
+    </div>
+
+      <ul className='app__navbar-links'>
+          {['home', 'purpose', 'pillars', 'transformation', 'team', 'contact'].map ((item) => (
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <a href={`#${item}`}>{item}</a>
+              <div />
+            </li>
+          ))}
+      </ul>
+  </nav>
   );
 };
 
