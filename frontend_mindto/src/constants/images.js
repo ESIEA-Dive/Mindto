09b21419974bc6c59ccbf3afa5ea9744741f07ae
Thaïs Planchenault <thais.planchenault@gmail.com>
2022-10-10 16:29:47 +0200

    fix css contact

--- frontend_mindto/src/constants/images.js
+++ frontend_mindto/src/constants/images.js
@@ -2,6 +2,7 @@ import logo from '../assets/logo.png';
 import header_video from '../assets/header_video.mp4';
 import apple_store from '../assets/apple_store.png';
 import google_play from '../assets/google_play.png';
+import contact from '../assets/contact.jpg';
 
 const imgs = {
     logo,
