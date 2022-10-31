bfdae04fa805483321067303da2e89b918708323
Enguerran <enguerran.huby@aroed.fr>
2022-10-31 15:21:30 +0100

    Footer v2 and Classes gradient
    Footer improved following the designer template and adding gradient color to classes section

--- frontend_mindto/src/constants/images.js
+++ frontend_mindto/src/constants/images.js
@@ -6,6 +6,7 @@ import contact from '../assets/contact.jpg';
 
 import arrow from '../assets/arrow.svg';
 import mindto_logo from '../assets/Mindto_logo.svg';
+import mindto_logo_green from '../assets/Mindto_logo_green.svg';
 import mindto_w from '../assets/mindto_wheel.svg';
 import mindto_s from '../assets/Mindto_solo.svg';
 import physical from '../assets/physical.jpg';
@@ -24,6 +25,7 @@ const imgs = {
     physical,
     arrow,
     mindto_logo,
+    mindto_logo_green,
     mindto_w,
     mindto_s,
     mindto_logo_card,
--- frontend_mindto/src/container/Footer/Footer.jsx
+++ frontend_mindto/src/container/Footer/Footer.jsx
@@ -1,54 +1,40 @@
 import React from 'react';
 import { BsFacebook, BsTwitter, BsInstagram } from "react-icons/bs";
-import { RiLinkedinFill, RiArrowUpCircleFill } from "react-icons/ri";
+import { RiLinkedinFill } from "react-icons/ri";
+import { images } from '../../constants';
 
 import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="footer-col">
-            <h4>Follow us</h4>
-            <p className='separation'></p>
-            <div className="social-links">
-              <a href="#"><BsFacebook className="logo" /></a>
-              <a href="#"><BsTwitter className="logo" /></a>
-              <a href="#"><BsInstagram className="logo" /></a>
-              <a href="#"><RiLinkedinFill className="logo" /></a>
-            </div>
-          </div>
-          <div className="footer-col">
-            <h4>Newsletter</h4>
-            <p className='separation'></p>
-            <div>
-              <form>
-                <label className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</label>
-                <div className="newsletter">
-                  <input placeholder='email...' type="email" id="email" name="email" />
-                  <div>
-                    <button className="btnsend">Subscribe</button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="footer-col">
-            <h4>Adress</h4>
-            <p className='separation'></p>
-            <div>
-              <p className="text">Siège social<br />
-                New York, Etats-Unis<br />
-                Adresse<br />
-                x xxxx x x , 12523 x xx</p>
-            </div>
-          </div>
+      <div className="footer-col">
+        <div className='footer-mindto-logo'>
+          <img src={images.mindto_logo_green} alt='mindto-logo' />
+        </div>
+        <p className="copyright">© 2022 by Mindto - All rights reserved</p>
+        <p className="mentionslegales"><a>Terms and conditions</a></p>
+      </div>
+      <div className="footer-col">
+        <h4>Follow us</h4>
+        <p className='separation'></p>
+        <div className="social-links">
+          <a href="#"><BsInstagram className="logo" /></a>
+          <a href="#"><RiLinkedinFill className="logo" /></a>
+          <a href="#"><BsFacebook className="logo" /></a>
+          <a href="#"><BsTwitter className="logo" /></a>
         </div>
       </div>
-      <button className="btn-arrow-up"><RiArrowUpCircleFill size={50} /></button>
-      <div className="subfooter">
-        © 2022 Mindto, Inc. All rights reserved · <span className='mentions-legales'>Mentions légales</span>
+      <div className="footer-col">
+        <h4>Newsletter</h4>
+        <p className='separation'></p>
+        <form>
+          <label>Sign up to receive MindTo news and updates.</label>
+          <div className="newsletter">
+            <input placeholder='Email...' type="email" id="email" name="email" />
+            <button className="btnsend">Subscribe</button>
+          </div>
+        </form>
       </div>
     </footer>
 
