584127bb49ae523c4311e2281a70b5338aa23b87
Enguerran <enguerran.huby@aroed.fr>
2022-11-07 18:30:02 +0100

    New testimonial section

--- frontend_mindto/src/container/Testimonial/Testimonial.jsx
+++ frontend_mindto/src/container/Testimonial/Testimonial.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import { AppWrap } from '../../wrapper';
+import images from '../../constants/images';
 
 import './Testimonial.scss';
+import { BsArrowBarDown } from "react-icons/bs";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -14,6 +16,7 @@ import "swiper/css/navigation";
 
 // import required modules
 import { Pagination, Navigation } from "swiper";
+import { motion } from "framer-motion"
 
 const Testimonial = () => {
   return (
@@ -30,43 +33,109 @@ const Testimonial = () => {
         className="testimonial-swiper"
       >
         <SwiperSlide className="testimonial-swiper-slide">
-        <div className='testimonial-quote'>
-            <p className='one'>
-              <span className='quote-symbol'>"</span>
-              <br></br>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              <br></br>
-              <br></br>
-              <span className='quote-author'>Guillaume Dive, MindTo Founder</span>
-            </p>
+          <div className='testimonial-quote'>
+            <div className='testimonial-left-column'>
+              <div className="line1"></div>
+              <div className="line2"></div>
+              <img className='testimonial-picture' src={images.testimonial_one} alt='logo'></img>
+              <p className='testimonial-name'>User One</p>
+              <p className='testimonial-job'>ESIEA Student</p>
+            </div>
+            <div className='testimonial-right-column'>
+              <p className='testimonial-quote-symbol'>"</p>
+              <div className='testimonial-quote-scroll-container' id="style-2">
+                <p className='testimonial-quote-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies vulputate orci non rhoncus. Donec pellentesque venenatis nunc, ac blandit ex vestibulum ut. Praesent eleifend accumsan tortor in ultricies. Mauris a tortor vitae metus tempus interdum sit amet ut turpis. Integer a egestas massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies vulputate orci non rhoncus. Donec pellentesque venenatis nunc, ac blandit ex vestibulum ut. Praesent eleifend accumsan tortor in ultricies. Mauris a tortor vitae metus tempus interdum sit amet ut turpis. Integer a egestas massa.</p>
+              </div>
+              <div className='testimonial-scroll-and-deco-container'>
+                <motion.div
+                  className='testimonial-quote-scroll-down-container'
+                  whileInView={{
+                    y: [-6, 6, -6],
+                    transition: { duration: 2, repeat: Infinity }
+                  }}
+                >
+                  <p className='testimonial-quote-scroll-down-text'>Scroll down</p>
+                  <BsArrowBarDown className='scroll-down-icon' />
+                </motion.div>
+                <div className='line3-to-5'>
+                  <div className="line3"></div>
+                  <div className="line4"></div>
+                  <div className="line5"></div>
+                </div>
+              </div>
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="testimonial-swiper-slide">
           <div className='testimonial-quote'>
-            <p className='two'>
-              <span className='quote-symbol'>"</span>
-              <br></br>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              <br></br>
-              <br></br>
-              <span className='quote-author'>Guillaume Dive, MindTo Founder</span>
-            </p>
+            <div className='testimonial-left-column'>
+              <div className="line1"></div>
+              <div className="line2"></div>
+              <img className='testimonial-picture' src={images.testimonial_one} alt='logo'></img>
+              <p className='testimonial-name'>User One</p>
+              <p className='testimonial-job'>ESIEA Student</p>
+            </div>
+            <div className='testimonial-right-column'>
+              <p className='testimonial-quote-symbol'>"</p>
+              <div className='testimonial-quote-scroll-container' id="style-2">
+                <p className='testimonial-quote-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies vulputate orci non rhoncus. Donec pellentesque venenatis nunc, ac blandit ex vestibulum ut. Praesent eleifend accumsan tortor in ultricies. Mauris a tortor vitae metus tempus interdum sit amet ut turpis. Integer a egestas massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies vulputate orci non rhoncus. Donec pellentesque venenatis nunc, ac blandit ex vestibulum ut. Praesent eleifend accumsan tortor in ultricies. Mauris a tortor vitae metus tempus interdum sit amet ut turpis. Integer a egestas massa.</p>
+              </div>
+              <div className='testimonial-scroll-and-deco-container'>
+                <motion.div
+                  className='testimonial-quote-scroll-down-container'
+                  whileInView={{
+                    y: [-6, 6, -6],
+                    transition: { duration: 2, repeat: Infinity }
+                  }}
+                >
+                  <p className='testimonial-quote-scroll-down-text'>Scroll down</p>
+                  <BsArrowBarDown className='scroll-down-icon' />
+                </motion.div>
+                <div className='line3-to-5'>
+                  <div className="line3"></div>
+                  <div className="line4"></div>
+                  <div className="line5"></div>
+                </div>
+              </div>
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide className="testimonial-swiper-slide">
           <div className='testimonial-quote'>
-            <p className='three'>
-              <span className='quote-symbol'>"</span>
-              <br></br>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              <br></br>
-              <br></br>
-              <span className='quote-author'>Guillaume Dive, MindTo Founder</span>
-            </p>
+            <div className='testimonial-left-column'>
+              <div className="line1"></div>
+              <div className="line2"></div>
+              <img className='testimonial-picture' src={images.testimonial_one} alt='logo'></img>
+              <p className='testimonial-name'>User One</p>
+              <p className='testimonial-job'>ESIEA Student</p>
+            </div>
+            <div className='testimonial-right-column'>
+              <p className='testimonial-quote-symbol'>"</p>
+              <div className='testimonial-quote-scroll-container' id="style-2">
+                <p className='testimonial-quote-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies vulputate orci non rhoncus. Donec pellentesque venenatis nunc, ac blandit ex vestibulum ut. Praesent eleifend accumsan tortor in ultricies. Mauris a tortor vitae metus tempus interdum sit amet ut turpis. Integer a egestas massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies vulputate orci non rhoncus. Donec pellentesque venenatis nunc, ac blandit ex vestibulum ut. Praesent eleifend accumsan tortor in ultricies. Mauris a tortor vitae metus tempus interdum sit amet ut turpis. Integer a egestas massa.</p>
+              </div>
+              <div className='testimonial-scroll-and-deco-container'>
+                <motion.div
+                  className='testimonial-quote-scroll-down-container'
+                  whileInView={{
+                    y: [-6, 6, -6],
+                    transition: { duration: 2, repeat: Infinity }
+                  }}
+                >
+                  <p className='testimonial-quote-scroll-down-text'>Scroll down</p>
+                  <BsArrowBarDown className='scroll-down-icon' />
+                </motion.div>
+                <div className='line3-to-5'>
+                  <div className="line3"></div>
+                  <div className="line4"></div>
+                  <div className="line5"></div>
+                </div>
+              </div>
+            </div>
           </div>
         </SwiperSlide>
       </Swiper>
-    </div>
+    </div >
   );
 };
 
