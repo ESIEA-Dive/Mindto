fc09c572f842a1eb6f16ff7b6bed8230f1511b29
Enguerran <enguerran.huby@aroed.fr>
2022-11-09 17:25:13 +0100

    New team section

--- frontend_mindto/src/container/Team/Team.jsx
+++ frontend_mindto/src/container/Team/Team.jsx
@@ -1,51 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { AppWrap } from '../../wrapper';
 import images from '../../constants/images';
+import { motion } from "framer-motion"
 
 import './Team.scss';
 
 const Team = () => {
+
+  const [status, setStatus] = useState(0);
+  const members = [
+    {
+      name: "Member ONE",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      job: "job one",
+      picture: images.testimonial_one,
+    },
+    {
+      name: "Member TWO",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      job: "job two",
+      picture: images.testimonial_two,
+    },
+    {
+      name: "Member THREE",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      job: "job three",
+      picture: images.testimonial_three,
+    },
+    {
+      name: "Member FOUR",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      job: "job four",
+      picture: images.testimonial_four,
+    },
+
+  ];
+
   return (
     <div className='team-container'>
       <p className='title'>
         Team
       </p>
       <div className='team-group'>
-        <div className='team-person'>
-          <div className='person-picture'>
-            <img className='person-picture-img' src={images.member} alt='member'></img>
-          </div>
+        {members.map((member, index) => (<motion.div
+          className='team-person'
+          whileInView={
+            status === index ?
+              { width: '46%', opacity: 1, transition: { duration: 0.5 } }
+              :
+              { width: '18%', opacity: 0.3, transition: { duration: 0.5 }, fontSize: 0.7 }
+          }
+          onClick={() => setStatus(index)}
+          onHoverStart={() => setStatus(index)}
+        >
+          <img className='person-picture-img' src={member.picture} alt='member'></img>
           <div className='person-bio'>
-            <p className='person-name'>Guillaume Dive</p>
-            <p className='person-status'>Founder</p>
+            <motion.div
+              whileInView={{
+                opacity: [0, 1],
+                transition: { duration: 1 }
+              }}
+              onClick={() => setStatus(index)}
+              onHoverStart={() => setStatus(index)}
+            >
+              <p className='person-name'>{member.name}</p>
+              <p className='person-status'>{member.job}</p>
+            </motion.div>
+            {status == index && (<motion.div
+              whileInView={{
+                opacity: [0, 1],
+                transition: { duration: 2 }
+              }}
+              onClick={() => setStatus(index)}
+              onHoverStart={() => setStatus(index)}
+            >
+              <p className='person-description'>{member.description}</p>
+            </motion.div>
+            )}
           </div>
-        </div>
-        <div className='team-person'>
-          <div className='person-picture'>
-            <img className='person-picture-img' src={images.member} alt='member'></img>
-          </div>
-          <div className='person-bio'>
-            <p className='person-name'>Guillaume Dive</p>
-            <p className='person-status'>Founder</p>
-          </div>
-        </div><div className='team-person'>
-          <div className='person-picture'>
-            <img className='person-picture-img' src={images.member} alt='member'></img>
-          </div>
-          <div className='person-bio'>
-            <p className='person-name'>Guillaume Dive</p>
-            <p className='person-status'>Founder</p>
-          </div>
-        </div><div className='team-person'>
-          <div className='person-picture'>
-            <img className='person-picture-img' src={images.member} alt='member'></img>
-          </div>
-          <div className='person-bio'>
-            <p className='person-name'>Guillaume Dive</p>
-            <p className='person-status'>Founder</p>
-          </div>
-        </div>
+        </motion.div>))}
       </div>
     </div>
   );
